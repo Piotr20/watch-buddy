cb5adb6d1cac2503b360dc577ce7304328b3b909
Wojo3214 <55576529+Wojo3214@users.noreply.github.com>
2024-11-03 14:15:30 +0100

    added carousel item

---
+++ components/theme/carousel/carouselItem.tsx
@@ -0,0 +1,76 @@
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { Image, StyleSheet, View } from 'react-native';
+import { ThemeText } from '../typography';
+
+
+type Props = {
+    type: "cast" | "gallery" | "movie";
+    heading: string,
+    subheading: string,
+    imgSrc: string,
+};
+
+export function CarouselItem({ type = 'movie', heading, subheading, imgSrc }:Props) {
+  const colors = useThemeColor();
+
+  const ItemStyles = StyleSheet.create({
+    common: {
+      paddingVertical: 2,
+      paddingHorizontal: 4,
+      borderRadius: 8,
+      display: 'flex',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderWidth: 1, 
+      borderColor: 'transparent',
+      gap: 4,
+    },
+    image: {
+        objectFit: 'cover',
+        ...(type === 'movie' ? { 
+            width: 150,
+            height: 176,
+          } : {
+            width: 120,
+            height: 120,
+          }
+        ),
+        ...(type === 'cast' ? { borderRadius: 999 } : { borderRadius: 12 })
+    },
+    infoContainer: {
+      width: '100%',
+      marginTop: 8,
+        ...(type === 'gallery' && { display: 'none' }
+        ),
+    },
+    heading: {
+      color: colors.text.base,
+      ...(type === 'cast' && { textAlign: 'center' })
+    },
+    subheading: {
+      color: colors.text.details,
+      marginBottom: 4,
+      ...(type === 'cast' && { textAlign: 'center' })
+    },
+  });
+
+  return (
+    <View style={{
+      width: '100%',
+      ...(type === 'movie' ? { 
+        maxWidth: 150,
+        } : { 
+          maxWidth: 120,
+        })
+    }}>
+        <Image style={ItemStyles.image} source={{
+          uri: imgSrc,
+        }}/> 
+        <View style={[ItemStyles.infoContainer]}>
+            <ThemeText size='base' style={ItemStyles.heading}>{heading}</ThemeText>
+            <ThemeText size={type === "movie" ? 'xs' : 'sm'} style={ItemStyles.subheading}>{subheading}</ThemeText>
+        </View>
+    </View>
+  );
+}
